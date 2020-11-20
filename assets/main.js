bb8c4a006f948f0101488d3741dc243f0cea03fc
FrancescoDale <fd060862@gmail.com>
2020-11-20 16:00:00 +0100

    chiamata API

--- assets/main.js
+++ assets/main.js
@@ -11,7 +11,8 @@ var app = new Vue({
         //finchè la lunghezza dell'array è minore di 10, un ciclo for effettua la chiamata e inserisce l'elemento nell'array
         if (this.mailList.lenght < 10) {
             for (var i = 0; i < 10; i++) {
-                
+                // richiamo da remoto una email
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             }
 
         }
