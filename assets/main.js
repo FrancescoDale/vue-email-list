4e2585d1bb07d723205e702603b011d90e3a452b
FrancescoDale <fd060862@gmail.com>
2020-11-20 15:58:17 +0100

    ciclo for incrementale

--- assets/main.js
+++ assets/main.js
@@ -10,6 +10,9 @@ var app = new Vue({
 
         //finchè la lunghezza dell'array è minore di 10, un ciclo for effettua la chiamata e inserisce l'elemento nell'array
         if (this.mailList.lenght < 10) {
+            for (var i = 0; i < 10; i++) {
+                
+            }
 
         }
 
