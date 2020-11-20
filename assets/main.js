f2bf6b0cc7614e733d88f9338fa6bbe57ea89d7a
FrancescoDale <fd060862@gmail.com>
2020-11-20 15:29:49 +0100

    ciclo che richiama 10 mail

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,18 @@
 var app = new Vue ({
     el : '#root',
     data : {
+        // array di email
         mailList : [],
     },
     methods : {},
+
+    mounted() {
+        // attraverso un ciclo andrò a richiamare 10 indirizzi email
+        for (var i = 10; i > 0; i--) {
+            //richiama le mail da remoto
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail'),
+
+        }
+    }
 
 });
