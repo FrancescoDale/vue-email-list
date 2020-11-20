3e868095d5a83a9a2cf5ad7d9cc5fdb0d613ff32
FrancescoDale <fd060862@gmail.com>
2020-11-20 16:44:52 +0100

    fix vari typo e virgole, spaziatura con bootstrap e risoluzione del problema principale invertendo le cdn nell'head

--- assets/main.js
+++ assets/main.js
@@ -7,18 +7,19 @@ var app = new Vue({
     methods : {},
 
     mounted() {
-
         //finchè la lunghezza dell'array è minore di 10, un ciclo for effettua la chiamata e inserisce l'elemento nell'array
-        if (this.mailList.lenght < 10) {
+        if (this.mailList.length < 10) {
             for (var i = 0; i < 10; i++) {
                 // richiamo da remoto una email
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            }
-
-        }
-
-
-    }
-
-
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    // push nell'array
+                    this.mailList.push(response.data.response);
+                });
+            };
+        };
+    },
 });
+
+// dopo molte prove e vari fix ( il classico typo in lenght), ho scoperto che l'unico modo per far funzionare il tutto era invertire le cdn nell'head nell'html
